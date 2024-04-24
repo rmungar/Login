28ffa6f60d539be1aca54633350f762943c63aeb
rmungar <mgraul@iesrafaelalberti.es>
2024-04-24 17:44:54 +0200

    Falta el codigo interno

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,65 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.singleWindowApplication
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun Login() {
+    MaterialTheme{
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+
+            OutlinedTextField(
+                value = "",
+                placeholder = { Text("Nombre de usuario") },
+                onValueChange = {}
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            OutlinedTextField(
+                value = "",
+                placeholder = { Text("Contraseña") },
+                visualTransformation = PasswordVisualTransformation(),
+                onValueChange = {},
+                modifier = Modifier
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .size(100.dp, 50.dp)
+            ){
+                Text("LOGIN")
+            }
         }
+
+
     }
+
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        Login()
     }
 }
