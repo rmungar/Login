d944b82958851d3e50d6fb2e8f9d98fc943eaeff
rmungar <mgraul@iesrafaelalberti.es>
2024-04-24 19:45:00 +0200

    Arreglar visibilidad

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,28 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.foundation.border
+import androidx.compose.material.icons.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
 
 @Composable
 @Preview
-fun Login() {
+fun LoginScreen(user:String, onEntrada: (String) -> Unit, psswd:String, onEntrada2: (String) -> Unit, estadoBoton:Boolean, onClick:(Boolean) -> Unit, psswdVisible:Boolean, onLogin : () -> Unit) {
+
     MaterialTheme{
 
         Column(
@@ -30,36 +32,88 @@ fun Login() {
             verticalArrangement = Arrangement.Center
         ){
 
-            OutlinedTextField(
-                value = "",
-                placeholder = { Text("Nombre de usuario") },
-                onValueChange = {}
-            )
+            Usuario(user,onEntrada)
+
             Spacer(modifier = Modifier.size(10.dp))
-            OutlinedTextField(
-                value = "",
-                placeholder = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                onValueChange = {},
-                modifier = Modifier
-            )
+
+            Psswd(psswd, psswdVisible, onEntrada2)
+
             Spacer(modifier = Modifier.size(10.dp))
-            Button(
-                onClick = {},
-                modifier = Modifier
-                    .size(100.dp, 50.dp)
-            ){
-                Text("LOGIN")
-            }
+
+            Boton(estadoBoton, onLogin)
         }
 
 
     }
 
 }
+@Composable
+fun Usuario(
+    user: String,
+    onEntrada : (String) -> Unit
+){
+    OutlinedTextField(
+        value = user,
+        placeholder = { Text("Nombre de usuario") },
+        onValueChange = { onEntrada(it) },
+        label = { Text("Usuario")}
+    )
+}
+
+@Composable
+fun Psswd(psswd: String, psswdVisible: Boolean, onEntrada2: (String) -> Unit){
+    OutlinedTextField(
+        value = psswd,
+        placeholder = { Text("Contraseña") },
+        visualTransformation = if (psswdVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        onValueChange = {onEntrada2(it)},
+        label = { Text("Contraseña")},
+        trailingIcon = {
+            IconToggleButton(
+                checked = psswdVisible,
+                onCheckedChange = {!psswdVisible}
+            ){
+                Icon(
+                    imageVector = if (psswdVisible) Icons.Default.VisibilityOff else Icons.Default.Visibility,
+                    contentDescription = null
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun Boton(estadoBoton: Boolean, onLogin: () -> Unit){
+    Button(
+        onClick = {
+            onLogin() },
+        enabled = estadoBoton,
+        modifier = Modifier
+            .size(100.dp, 50.dp)
+    ){
+        Text("LOGIN")
+    }
+}
 
 fun main() = application {
+    var user by remember { mutableStateOf("") }
+    var psswd by remember { mutableStateOf("") }
+    var estadoBoton = user.isNotBlank() && psswd.isNotBlank()
+    var psswdVisible by remember { mutableStateOf(false)}
+
     Window(onCloseRequest = ::exitApplication) {
-        Login()
+        LoginScreen(
+            user,
+            onEntrada = {user = it },
+            psswd,
+            onEntrada2 = {psswd = it},
+            estadoBoton,
+            onClick = {estadoBoton = it},
+            psswdVisible,
+            onLogin = {
+                user = ""
+                psswd = ""
+            }
+        )
     }
 }
